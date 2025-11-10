ee50cf81034acbea16bbd066dc89835c17ed3da7
Adam Brandt <57259688+adkadt@users.noreply.github.com>
2025-11-10 00:37:32 -0500

    added header

---
+++ frontend/src/components/AppHeader.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+import "./AppHeader.css"
+
+import ActiveUsername from "./ActiveUsername";
+import Logout from "./Logout"
+
+
+function AppHeader()
+{
+    return (
+        <header id="roulette-header">
+            <ActiveUsername />
+            <Logout />
+        </header>
+    )
+}
+
+export default AppHeader;
