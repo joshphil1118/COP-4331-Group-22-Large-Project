98c7dc695bd07c69be056fec6a9f3569b2d7ff65
Adam Brandt <57259688+adkadt@users.noreply.github.com>
2025-11-10 02:29:40 -0500

    username component

--- frontend/src/components/ActiveUsername.tsx
+++ frontend/src/components/ActiveUsername.tsx
@@ -1,8 +1,21 @@
+import { useState, useEffect } from "react";
+
 function ActiveUsername()
 {
+    const [username, setUsername] = useState('')
+
+    useEffect(() => {
+        const userData = localStorage.getItem("user_data")
+        if (userData)
+        {
+            const user = JSON.parse(userData)
+            setUsername(user.firstName + ' ' + user.lastName)
+        }
+    }, []);
+
     return(
       <div id="username-div">
-        <span id="user-name">John Doe</span><br />
+        <span id="user-name">{username}</span>
       </div>
     );
 };
