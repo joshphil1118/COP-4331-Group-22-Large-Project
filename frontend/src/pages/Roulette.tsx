c5b306b1ef7abf94cd9bb93d00c27202bd709310
Adam Brandt <57259688+adkadt@users.noreply.github.com>
2025-11-10 00:38:38 -0500

    added Roulette page

---
+++ frontend/src/pages/Roulette.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+
+import AppHeader from "../components/AppHeader";
+
+import "./Roulette.css"
+
+function Roulette()
+{
+    // const [balance, setBalance] = useState(1000);
+    // const [bets, setBets] = useState
+
+    return (
+        <div id="roulette-container">
+            <AppHeader />
+        </div>
+    )
+}
+
+export default Roulette;
