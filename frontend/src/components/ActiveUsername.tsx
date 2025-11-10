59d8c97f971b2f19a4ce698a076d9dd01320557b
Adam Brandt <57259688+adkadt@users.noreply.github.com>
2025-11-10 00:37:10 -0500

    TODO: add active username

---
+++ frontend/src/components/ActiveUsername.tsx
@@ -0,0 +1,10 @@
+function ActiveUsername()
+{
+    return(
+      <div id="username-div">
+        <span id="user-name">John Doe</span><br />
+      </div>
+    );
+};
+
+export default ActiveUsername;
